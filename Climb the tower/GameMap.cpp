7fbf4b12f7f4a67d9793bcedd5f91b0f5e36acfd
maoap1 <maoap1@gmail.com>
2018-03-11 09:18:22 +0100

    Created Map (needed for AI)

--- Climb the tower/GameMap.cpp
+++ Climb the tower/GameMap.cpp
@@ -11,6 +11,17 @@ namespace GameMap
 	list<ActiveGameObject*> Movables;
 	list<Collider*> Colliders;
 
+	/// <summary>
+	/// The game map. Stores 0 for empty space, -1 for walls;  
+	/// Imagine we would like to work with Map[i][j].  
+	/// Then coordinates of top left corner of this are:
+	/// x = (j + 1)*WALL_SIZE;
+	/// y = (i + 1)*WALL_SIZE;
+	/// </summary>
+	vector<vector<int>> Map;
+	int Map_height;
+	int Map_width;
+
 	void FirstGameMapInicialization(int map_width, int map_height)
 	{
 
@@ -30,9 +41,44 @@ namespace GameMap
 
 		//Walls:
 		Colliders.push_back(new Collider(0, 0, map_height*WALL_SIZE, WALL_SIZE));							// Western wall
-		Colliders.push_back(new Collider(WALL_SIZE, 0, WALL_SIZE, map_width*WALL_SIZE));						// Northern wall
+		Colliders.push_back(new Collider(WALL_SIZE, 0, WALL_SIZE, map_width*WALL_SIZE));					// Northern wall
 		Colliders.push_back(new Collider(eastern_wall_x, WALL_SIZE, map_height*WALL_SIZE, WALL_SIZE));		// Eastern wall
 		Colliders.push_back(new Collider(WALL_SIZE, southern_wall_y, WALL_SIZE, map_width*WALL_SIZE));		// Southern wall
+
+
+		Map_height = map_height - 2;
+		Map_width = map_width - 2;
+
+		Map.resize(Map_height);
+		for (int i = 0; i < Map_height; i++)
+			Map[i].resize(Map_width);
+
+		for (int i = 0; i < Map_height; i++)
+		{
+			for (int j = 0; j < Map_width; j++)
+			{
+				Map[i][j] = 0;
+			}
+		}
+
+		Map[3][5] = 1;
+		Map[3][6] = 1;
+		Map[3][7] = 1;
+		Map[4][6] = 1;
+		Map[5][6] = 1;
+		Map[6][6] = 1;
+
+		for (int i = 0; i < Map_height; i++)
+		{
+			for (int j = 0; j < Map_width; j++)
+			{
+				if (Map[i][j] == 1)
+				{
+					Drawables.push_back(new Wall("Resources/image.png", (j + 1)*WALL_SIZE, (i + 1)*WALL_SIZE));
+					Colliders.push_back(new Collider((j + 1)*WALL_SIZE, (i + 1)*WALL_SIZE, WALL_SIZE, WALL_SIZE));
+				}
+			}
+		}
 
 	}
 
--- Climb the tower/GameMap.h
+++ Climb the tower/GameMap.h
@@ -11,9 +11,11 @@ namespace GameMap
 	extern list<ActiveGameObject*> Movables;
 	extern list<Collider*> Colliders;
 
+	extern vector<vector<int>> Map;
+
 	void FirstGameMapInicialization(int map_width, int map_height);
 	void CreateSpell(float x, float y, int orientation, int spellID);
-	void CreateEnemy(float x, float y, int orientation, int spellID);
+	void CreateEnemy(float x, float y, int orientation);
 
 
 
